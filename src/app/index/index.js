a84b1a5d5d1504bc070511fea5c8be0fe2ce66de
Jaymz Rhime <jrhime@rubicon.com>
2015-02-10 14:40:46 -0800

    Add a view and some tests for square and rectangle services

--- src/app/index/index.js
+++ src/app/index/index.js
@@ -1,20 +1,27 @@
 angular.module('index', [
-  'ui.router'
+  'ui.router',
+  'common'
 ])
-.config(function($stateProvider) {
-  $stateProvider.state('index', {
-    url: '/',
-    views: {
-      main: {
-        controller: 'IndexController',
-        templateUrl: 'index/index.tpl.html'
+  .config(function($stateProvider) {
+    $stateProvider.state('index', {
+      url: '/',
+      views: {
+        main: {
+          controller: 'IndexController',
+          templateUrl: 'index/index.tpl.html'
+        }
+      },
+      data: {
+        pageTitle: 'Home'
       }
-    },
-    data: {
-      pageTitle: 'Home'
-    }
+    });
+  })
+  .controller('IndexController', function($scope) {
+
+  })
+  .value('rectangleFactory', function() {
+
+  })
+  .value('squareFactory', function() {
+
   });
-})
-.controller('IndexController', function($scope) {
-  $scope.hello = 'Hello world';
-});
--- src/app/index/index.spec.js
+++ src/app/index/index.spec.js
@@ -1,17 +1,36 @@
-describe('IndexController', function() {
-  var indexController;
-  var $scope;
-
+describe('index', function() {
   beforeEach(module('index'));
 
-  beforeEach(inject(function($controller, $rootScope) {
-    $scope = $rootScope.$new();
-    indexController = $controller('IndexController', {
-      $scope: $scope
-    });
-  }));
+  describe('controller', function() {
+    it('should have shapes', inject(function($controller) {
+      var scope = {};
+      var indexController = $controller('IndexController', {
+        $scope: scope
+      });
 
-  it('should say hello', inject(function () {
-    expect($scope.hello).to.exist;
-  }));
+      expect(scope).to.have.property('rectangle');
+      expect(scope.rectangle).to.respondTo('area');
+      expect(scope).to.have.property('square');
+      expect(scope.square).to.respondTo('area');
+    }));
+  });
+
+  describe("rectangle", function(){
+    it("should calculate area", inject(function(rectangleFactory){
+      var rectangle = rectangleFactory();
+      rectangle.width = 4;
+      rectangle.height = 5;
+
+      expect(rectangle.area()).to.equal(20);
+    }));
+  });
+
+  describe("square", function(){
+    it("should calculate area", inject(function(squareFactory){
+      var square = squareFactory();
+      square.size = 5;
+
+      expect(square.area()).to.equal(25);
+    }));
+  });
 });
